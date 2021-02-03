93e56ed8fdcbaff09b95ac70def4eaf09e61baa5
LiqunZhao <1347510570@qq.com>
2021-02-03 11:58:16 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,19 @@
+package lecture1;
+
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		while (x<10) {
+			System.out.println(x);
+			x = x + 1;
+		}
+	}
+
+}
+
+/*
+1. Before the variable is used in Java, it must be declared in advance;
+2. Java variables must have a specific type;
+3. Java variables types cannot be changed afterwards;
+4. Types are verified before even before the codes run.
+*/
---
+++ lab1/HelloWorld.java
@@ -0,0 +1,17 @@
+package lecture1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
+
+/*
+1. All code in Java must be part of a class;
+2. We delimit the beginning and end of the segments of code
+using { and };
+3. All statements in Java must end in a semi-colon;
+4. For code to run need public static void main(String[] args)
+
+*/
